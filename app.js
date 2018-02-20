9c535b07dd5a519afe8dbb0fb0a6e7f459547f29
Yuval Allweil <themarquisdesheric@gmail.com>
2018-02-20 15:32:58 -0800

    drumkit working pretty well! need to style it

--- app.js
+++ app.js
@@ -6,8 +6,19 @@ const playSound = e => {
 
   drum.classList.add('playing');
 
+  // reset sample to beginning in case key is pressed quickly
   audio.currentTime = 0;
   audio.play();
 }
+
+const removeTransitionEffect = e => {
+  if (e.propertyName !== 'transform') return;
+
+  e.target.classList.remove('playing');
+}
+
+// turn nodeList into an array
+const drums = [...document.querySelectorAll('.drum')];
+drums.forEach(drum => drum.addEventListener('transitionend', removeTransitionEffect));
 
 window.addEventListener('keydown', playSound);
