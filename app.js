9ccfb6a101c23415a7125b8e58338fb667d9e96e
Yuval Allweil <themarquisdesheric@gmail.com>
2018-02-20 15:19:59 -0800

    transition effect added, but need to have it revert to normal once complete

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const playSound = e => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const drum = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
   if (!audio) return;
+
+  drum.classList.add('playing');
 
   audio.currentTime = 0;
   audio.play();
