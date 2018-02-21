19257f1fd55d6a565df8b8bad3fe06b0be31bd51
Yuval Allweil <themarquisdesheric@gmail.com>
2018-02-21 09:55:39 -0800

    each drum cycles through different colors... not sure if I like it

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
+
+// make background change to a random color each keystroke
+
+// assign random color to each drum instead of cycling at random? 
+
 const playSound = e => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const drum = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
   if (!audio) return;
 
-  drum.classList.add('playing');
+  drum.classList.add(getRandomColor());
 
-  // reset sample to beginning in case key is pressed quickly
+  // reset sample to beginning in case trigger is pressed too quickly
   audio.currentTime = 0;
   audio.play();
 }
 
 const removeTransitionEffect = e => {
+  const [drum, ...rest] = e.target.classList;
+
   if (e.propertyName !== 'transform') return;
 
-  e.target.classList.remove('playing');
+  // sometimes several classes get added if trigger is pressed too quickly, so remove all classes but the first
+  e.target.classList.remove(...rest);
+}
+
+const getRandomColor = () => {
+  const colors = ['pastel-red', 'pastel-orange', 'pastel-yellow', 'pastel-green', 'pastel-blue'];
+  const randomInt = Math.floor(Math.random() * colors.length);
+
+  return colors[randomInt];
 }
 
 // turn nodeList into an array
